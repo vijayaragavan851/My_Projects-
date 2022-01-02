655fd1159b9fe1ec5d2d75243cd7f09e3cad7192
vijayaragavan851 <92657090+vijayaragavan851@users.noreply.github.com>
2022-01-02 12:39:11 +0530

    Update 5.Generating _The_ Random_Password_By _The_Length.py
    Formatting our code

--- 5.Generating _The_ Random_Password_By _The_Length.py
+++ 5.Generating _The_ Random_Password_By _The_Length.py
@@ -1,5 +1,5 @@
-print("random password generator.") # on 23.12.2021
 import random
+
 pass_len = int(input("Enter the length of the password:"))
 char = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFHIJKLMNOPQRSTUVWXZ!@#$%^*()"
 print(''.join(random.sample(char,pass_len)))
