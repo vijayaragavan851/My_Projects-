56b262a56487a2c9349d86713217e83cdb34019e
vijayaragavan851 <92657090+vijayaragavan851@users.noreply.github.com>
2022-01-02 12:25:36 +0530

    Update 2.Die_Roll.py

--- 2.Die_Roll.py
+++ 2.Die_Roll.py
@@ -1,10 +1,13 @@
 import random
+
 count = 0
 total_point = 0
+
 while int(input("press 1 to roll the die or 0 to exist:")):
     n = random.randint(1,6)
     total_point += n 
     print("you got",n)
     count += 1
+    
 print(f"you roll the die for {count} times.\nyour total score {total_point}")
  
